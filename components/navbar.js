57787b4dce45001a985f496327d31583c5100d99
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-17 12:33:22 +0530

    navbar imported succesfully

---
+++ components/navbar.js
@@ -0,0 +1,73 @@
+function navbar(){
+    return `<div id="navbar1">
+    <div>
+        <a href="#">FIND A CONSULTANT</a>
+    </div>
+    <div>
+        <a href="./index.html"><img src="https://www.rodanandfields.com/en-us/medias/rf-logo.svg?context=bWFzdGVyfGltYWdlc3w3MzczfGltYWdlL3N2Zyt4bWx8aW1hZ2VzL2g2MC9oYWIvODgyMjE0NzE4NjcxOC5zdmd8YjNmMmU2YTg5MTM0NTMzM2Y2ODg2ZmRkZTJhNmY2OWZhYmYyYjk5NWQxODkxODFiYjVkY2MxY2NjOWRlMzA5OA" alt=""></a>
+    </div>
+    <div id="navbar2">
+        <div>
+            <a href="#" id="search">Search</a>
+            <span class="material-icons">
+                search
+            </span>
+        </div>
+        <div>
+            <a href="./signin.html" id="signin">Sign in</a>
+            <span class="material-icons">
+                person
+            </span>
+        </div>
+        <div>
+            <span class="material-icons">
+                production_quantity_limits
+            </span>
+        </div>
+        <div>
+            English
+        </div>
+    </div>
+    
+</div>
+<hr>
+<ul id="navbar3">
+   <div>
+       <li>
+           <a href="#"><span>SHOP</span></a>
+       </li>
+   </div> 
+   <div>
+    <li>
+        <a href="#"><span>BEST SELLERS</span></a>
+    </li>
+</div>
+<div>
+    <li>
+        <a href="#"><span>OUR STORY</span></a>
+    </li>
+</div>
+<div>
+    <li>
+        <a href="#"><span>PERKS PROGRAM</span></a>
+    </li>
+</div>
+<div>
+    <li>
+        <a href="#"><span>BECOME A CONSULTANT</span></a>
+    </li>
+</div>
+<div>
+    <li>
+        <a href="#"><span>BLOG</span></a>
+    </li>
+</div>
+<div>
+    <li>
+        <a href="#"><span></span></a>
+    </li>
+</div>
+</ul>`
+}
+
+export default navbar
