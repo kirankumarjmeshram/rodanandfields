491d45b71a93c022293cab27faa5f7b160aa984b
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2021-12-17 16:33:12 +0530

    footer complited

---
+++ components/footer.js
@@ -0,0 +1,48 @@
+function footer(){
+    return ` <hr>
+    <div id="footer1">
+        <div  class="footer1-1">
+            <div>
+             <p>ABOUT R&F</p>
+             <ul>
+                 <li><a href="#">PRESS ROOM</a></li>
+                 <li><a href="#">CONPANY INFO</a></li>
+                 <li><a href="#">EXECUIVE TEAM</a></li>
+                 <li><a href="#">CAREERS</a></li>
+                 <li><a href="#">R+F RECYCLING PROGRAM</a></li>
+                 <li><a href="#">BLOG</a></li>
+             </ul>
+            </div>
+            <div>
+                <P>HELP</P>
+                <ul>
+                    <li><a href="#">PRODUCT AND HELP CENTER</a></li>
+                    <li><a href="#">SHIPPING</a></li>
+                    <li><a href="#">SATISFACTION GURANTEE</a></li>
+                    <li><a href="#">PROMOTIONAL TERMS & CONDITIONS</a></li>
+                    <li><a href="#">R+F FACTS</a></li>
+                    <li><a href="#">KLARNA</a></li>
+                    <li><a href="#">SITE MAP</a></li>
+                </ul>
+            </div>
+            <div class="footer1-2">
+                <div id="footer1-2-1">Let's Get Social</div>
+                <ul>
+                 <a href="#" class="fa fa-facebook"></a>
+                 <a href="#" class="fa fa-twitter"></a>
+                 <a href="#" class="fa fa-instagram"></a>
+                 <a href="#" class="fa fa-youtube"></a>
+                  <a href="#" class="fa fa-pinterest"></a>
+                </ul>
+            </div>
+            
+        </div>
+    </div>
+ <hr>
+ <div id="footer2">
+    © 2021 Rodan & Fields
+Privacy Policy | Terms & Conditions | Accessibility | California Supply Chains Act | DSA Code Of Ethics | Disclaimer |Preference Center
+ </div>`
+}
+
+export default footer
